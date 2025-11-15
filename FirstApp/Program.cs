17d9be81aa5b066c9d86799b54e9b05f58b3536b
jahangir <jahangirjm@BETA02>
2025-11-15 19:03:36 +0400

    Add project files.

---
+++ FirstApp/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Start Program");
+Console.WriteLine("Enter first value");
+int a=int.Parse(Console.ReadLine());
+Console.WriteLine("Enter second value");
+int b=int.Parse(Console.ReadLine());
+int c = a + b;
+Console.WriteLine("Result"+c);
