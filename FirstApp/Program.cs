41c485e35829483a80ced2a89c80cc2c081c7dfc
jahangir <jahangirjm@code.edu.az>
2025-11-15 20:06:37 +0400

    changed something

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,8 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Start Program");
-Console.WriteLine("Enter first value");
-int a=int.Parse(Console.ReadLine());
-Console.WriteLine("Enter second value");
-int b=int.Parse(Console.ReadLine());
-int c = a + b;
-Console.WriteLine("Result"+c);
+Console.WriteLine("Start the program dude");
+Console.WriteLine("Enter Your First Value");
+int a=int.Parse(Console.ReadLine()!);
+Console.WriteLine("Enter Your Second Value");
+int b=int.Parse(Console.ReadLine()!);
+int c=a+b;
+Console.WriteLine("Your Result is " + c);
+Console.WriteLine("End the program dude");
